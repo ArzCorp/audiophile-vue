c7389c9c20caec708b230993b73a7a8c04e83d7c
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-05-08 11:43:26 -0500

    💄 added container

---
+++ src/components/Container.vue
@@ -0,0 +1,9 @@
+<template>
+	<main class="w-full max-w-[80%] font-manrope mx-auto">
+		<slot></slot>
+	</main>
+</template>
+
+<script>
+export default {}
+</script>
--- src/components/Layout.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<main class="w-full max-w-[80%]">
-		<slot></slot>
-	</main>
-</template>
