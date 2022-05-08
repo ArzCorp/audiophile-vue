3253985a7c4444b565d967e5c08f0f5c7ea2bcc2
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-05-08 11:43:42 -0500

    💄 improved styles

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,3 +1,7 @@
 <template>
-	<button>Button</button>
+	<button
+		class="min-w-[160px] py-4 px-2 bg-[#D87D4A] text-xs font-bold text-center"
+	>
+		Button
+	</button>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,16 +2,19 @@
 import Logo from './Logo.vue'
 import Navbar from './Navbar.vue'
 import Cart from './Cart.vue'
+import Container from './Container.vue'
 </script>
 
 <template>
 	<header
 		class="bg-black text-white w-full mx-auto py-9 border-b border-[#979797]"
 	>
-		<div class="w-full max-w-[80%] mx-auto flex items-center justify-between">
-			<Logo />
-			<Navbar />
-			<Cart />
-		</div>
+		<Container>
+			<div class="flex items-center justify-between">
+				<Logo />
+				<Navbar />
+				<Cart />
+			</div>
+		</Container>
 	</header>
 </template>
