0a8961078bf4dd0536ecd5ed59cc9102069853d1
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-05-10 07:42:12 -0500

    🐛 fixed navbar styles

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,9 @@
 		<ul
 			class="fixed lg:static top-0 left-0 bg-black min-w-[200px] min-h-screen lg:min-h-min px-3 lg:px-0 py-5 lg:py-0 flex-col lg:flex-row flex lg:items-center gap-8 text-sm"
 		>
-			<i class="fas fa-angle-double-left absolute top-3 right-3" />
+			<div class="lg:hidden">
+				<i class="fas fa-angle-double-left absolute top-3 right-3" />
+			</div>
 			<li>HOME</li>
 			<li>HEADPHONES</li>
 			<li>SPEAKERS</li>
