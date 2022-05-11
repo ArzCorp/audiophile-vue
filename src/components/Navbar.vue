06411e87a77dd9ea751f376629e0f11d7442d099
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-05-11 07:25:11 -0500

    💄 improved responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 	<nav>
 		<ul
-			class="fixed lg:static top-0 left-0 bg-black min-w-[200px] min-h-screen lg:min-h-min px-3 lg:px-0 py-5 lg:py-0 flex-col lg:flex-row flex lg:items-center gap-8 text-sm"
+			class="fixed z-[80] lg:static top-0 left-0 bg-black min-w-[200px] min-h-screen lg:min-h-min px-3 lg:px-0 py-5 lg:py-0 flex-col lg:flex-row flex lg:items-center gap-8 text-sm"
 		>
 			<div class="lg:hidden">
 				<i class="fas fa-angle-double-left absolute top-3 right-3" />
