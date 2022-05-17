9b381fad1d78da092f95e919befe6a008bb290a1
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-05-17 09:18:16 -0500

    📱 added tabs responsive

--- src/components/home/Tabs.vue
+++ src/components/home/Tabs.vue
@@ -1,12 +1,16 @@
 <template>
 	<Container>
-		<ul class="flex gap-7 justify-center pt-24 pb-40">
+		<ul
+			class="flex flex-col md:flex-row gap-y-16 md:gap-y-0 xl:gap-7 justify-between xl:justify-center pt-24 pb-24 xl:pb-40"
+		>
 			<li v-for="tab in tabs">
 				<div
 					:key="tab.id"
-					class="bg-[#F1F1F1] flex justify-center pb-8 pt-28 relative min-h-[205px] w-[350px]"
+					class="bg-[#F1F1F1] mx-auto flex justify-center pb-5 xl:pb-8 pt-16 xl:pt-28 relative w-full lg:min-h-[165px] xl:min-h-[205px] md:w-[225px] xl:w-[350px]"
 				>
-					<figure class="mx-auto w-[125px] h-[170px] absolute -top-[50px]">
+					<figure
+						class="mx-auto w-[90px] xl:w-[125px] h-[115px] xl:h-[170px] absolute -top-[50px]"
+					>
 						<img
 							class="mx-auto w-full h-full object-contain"
 							:src="tab.img"
@@ -14,7 +18,9 @@
 						/>
 					</figure>
 					<div>
-						<h3 class="text-center font-bold text-lg mb-3">{{ tab.name }}</h3>
+						<h3 class="text-center font-bold text-base xl:text-lg mb-3">
+							{{ tab.name }}
+						</h3>
 						<p class="text-center text-xs text-black text-opacity-50">
 							Shop <i class="ml-1 fas fa-chevron-right text-[#D87D4A]" />
 						</p>
