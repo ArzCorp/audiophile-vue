f765374bc8fd19dc9e8430a02b9d4d38fb083495
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-05-18 07:09:15 -0500

    🎨 added home structure

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Header from './components/Header.vue'
 import Banner from './components/home/Banner.vue'
 import Tabs from './components/home/Tabs.vue'
+import Products from './components/home/Products.vue'
 </script>
 
 <template>
 	<Header />
 	<Banner />
 	<Tabs />
+	<Products />
 </template>
---
+++ src/components/home/Products.vue
@@ -0,0 +1,48 @@
+<template>
+	<section>
+		<div>
+			<figure>
+				<img
+					src="src/assets/img/home/desktop/image-speaker-zx9.png"
+					alt="speaker-zx9"
+				/>
+			</figure>
+			<div>
+				<p>ZX9 SPEAKER</p>
+				<p>
+					Upgrade to premium speakers that are phenomenally built to deliver
+					truly remarkable sound.
+				</p>
+				<Button>See Product</Button>
+			</div>
+		</div>
+		<div>
+			<div>
+				<p>ZX7 SPEAKER</p>
+				<Button>See Product</Button>
+			</div>
+			<figure>
+				<img
+					src="src/assets/img/home/desktop/image-speaker-zx7.jpg"
+					alt="speaker-zx7"
+				/>
+			</figure>
+		</div>
+		<figure>
+			<img
+				src="src/assets/img/home/desktop/image-earphones-yx1.jpg"
+				alt="image-earphones-yx1"
+			/>
+		</figure>
+		<div>
+			<p>YX1 EARPHONES</p>
+			<Button>YX1 EARPHONES</Button>
+		</div>
+	</section>
+</template>
+
+<script>
+import Button from '../Button.vue'
+
+export default { components: { Button } }
+</script>
