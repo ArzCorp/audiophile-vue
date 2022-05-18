c04d3b71a8c84ba7bac8a464841b15403b071903
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-05-18 07:18:44 -0500

    💄 added states in button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,11 +1,36 @@
 <template>
 	<button
-		class="min-w-[160px] tracking-[1px] py-4 px-2 bg-[#D87D4A] text-xs font-bold text-center"
+		:class="[
+			black,
+			ghost,
+			'min-w-[160px] tracking-[1px] py-4 px-2 text-xs font-bold text-center',
+		]"
 	>
 		<slot></slot>
 	</button>
 </template>
 
 <script>
-export default {}
+export default {
+	props: {
+		black: Boolean,
+		ghost: Boolean,
+	},
+	computed: {
+		black() {
+			if (this.black) {
+				return 'bg-black text-white'
+			} else {
+				return 'bg-[#D87D4A] text-white'
+			}
+		},
+		ghost() {
+			if (this.ghost) {
+				return 'bg-transparent text-black border border-black'
+			} else {
+				return ''
+			}
+		},
+	},
+}
 </script>
