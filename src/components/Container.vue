96af4711d98680c507cb41f0595a8ff6a86f0119
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-05-20 09:48:29 -0500

    🚧 1024 and 720px products responsive

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,7 +1,7 @@
 <template>
 	<main
 		style="display: inherit"
-		class="w-full max-w-[90%] lg:max-w-[80%] font-manrope mx-auto"
+		class="w-full max-w-[90%] lg:max-w-[80%] font-manrope mx-auto xl:max-w-screen-xl"
 	>
 		<slot></slot>
 	</main>
--- src/components/home/Products.vue
+++ src/components/home/Products.vue
@@ -2,26 +2,37 @@
 	<section>
 		<Container>
 			<div class="gap-y-12 gap-x-8 grid grid-cols-2 pb-36">
-				<div class="col-span-2 flex bg-[#D87D4A] text-white pt-24 h-[560px]">
-					<figure class="h-full min-w-max mt-auto ml-28">
-						<img
-							class="h-full"
-							src="src/assets/img/home/desktop/image-speaker-zx9.png"
-							alt="speaker-zx9"
-						/>
-					</figure>
-					<div class="ml-36 mr-24">
-						<p class="font-bold text-5xl mb-6">
-							ZX9 <br />
-							SPEAKER
-						</p>
-						<p class="font-medium text-sm text-white text-opacity-75 mb-16">
-							Upgrade to premium speakers that are phenomenally built to deliver
-							truly remarkable sound.
-						</p>
-						<Button black>See Product</Button>
+				<div class="bg-[#D87D4A] col-span-2">
+					<div
+						class="max-w-[900px] block lg:flex text-white pt-[52px] lg:pt-24 h-full lg:h-[560px]"
+					>
+						<figure
+							class="h-[237px] lg:h-full w-full xl:min-w-max mt-auto ml-0 lg:ml-8 xl:ml-28"
+						>
+							<img
+								class="h-full mx-auto xl:mx-0"
+								src="src/assets/img/home/desktop/image-speaker-zx9.png"
+								alt="speaker-zx9"
+							/>
+						</figure>
+						<div
+							class="text-center lg:text-left ml-14 xl:ml-36 mr-8 xl:mr-24 pb-16 lg:pb-0"
+						>
+							<p class="mt-16 lg:mt-0 font-bold text-5xl mb-6">
+								ZX9 <br />
+								SPEAKER
+							</p>
+							<p
+								class="font-medium text-sm text-white text-opacity-75 mb-10 lg:mb-16"
+							>
+								Upgrade to premium speakers that are phenomenally built to
+								deliver truly remarkable sound.
+							</p>
+							<Button black>See Product</Button>
+						</div>
 					</div>
 				</div>
+
 				<div class="relative col-span-2">
 					<div class="absolute top-20 left-24">
 						<p class="mb-8 font-bold text-[28px]">ZX7 SPEAKER</p>
