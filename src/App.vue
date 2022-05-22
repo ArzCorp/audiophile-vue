5c5d52a9ad06fc86eb1214fea3b0ed393f1f04a7
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-05-22 09:45:42 -0500

    💄 added home description

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Header from './components/Header.vue'
 import Banner from './components/home/Banner.vue'
 import Tabs from './components/home/Tabs.vue'
 import Products from './components/home/Products.vue'
+import Description from './components/home/Description.vue'
 </script>
 
 <template>
@@ -10,4 +11,5 @@ import Products from './components/home/Products.vue'
 	<Banner />
 	<Tabs />
 	<Products />
+	<Description />
 </template>
---
+++ src/components/home/Description.vue
@@ -0,0 +1,31 @@
+<template>
+	<section class="flex pb-52">
+		<Container>
+			<div class="mr-32 w-6/12">
+				<h2 class="text-[40px] font-bold uppercase mb-8">
+					Bringing you the<br />
+					<span class="text-[#D87D4A]">best</span> audio gear
+				</h2>
+				<p class="text-black text-opacity-50 text-sm w-full 2xl:w-10/12">
+					Located at the heart of New York City, Audiophile is the premier store
+					for high end headphones, earphones, speakers, and audio accessories.
+					We have a large showroom and luxury demonstration rooms available for
+					you to browse and experience a wide range of our products. Stop by our
+					store to meet some of the fantastic people who make Audiophile the
+					best place to buy your portable audio equipment.
+				</p>
+			</div>
+			<figure class="w-6/12">
+				<img
+					class="w-full"
+					src="src/assets/img/shared/desktop/image-best-gear.jpg"
+					alt="banner"
+				/>
+			</figure>
+		</Container>
+	</section>
+</template>
+<script>
+import Container from '../Container.vue'
+export default { components: { Container } }
+</script>
